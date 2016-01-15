479035848fa9d48939efd10ca23dffdab74df86b
dmssargent <dmssargent@yahoo.com>
2016-01-15 17:39:46 -0600

    FRC Talon Test

--- src/main/java/org/omegafactor/robot/Robot.java
+++ src/main/java/org/omegafactor/robot/Robot.java
@@ -1,9 +1,36 @@
 package org.omegafactor.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Robot extends IterativeRobot {
-    Robot
+
+    private final Talon test;
+
+    public Robot() {
+        test = new Talon(2);
+    }
+
+    @Override
+    public void robotInit() {
+        System.out.print("Hi!");
+    }
+
+    @Override
+    public void teleopInit() {
+        System.err.println("TeleOp Called");
+       test.set(1);
+   }
+
+    @Override
+    public  void disabledInit() {
+        test.set(0);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        System.out.println("TeleOpP Called");
+        test.set(.9);
+    }
 }
 
