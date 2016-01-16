e934a5d66419fa02cfe39b1bc209184d425100b0
dmssargent <dmssargent@yahoo.com>
2016-01-15 18:02:20 -0600

    FRC Beginning of the Services

--- src/main/java/org/omegafactor/robot/Robot.java
+++ src/main/java/org/omegafactor/robot/Robot.java
@@ -1,14 +1,25 @@
 package org.omegafactor.robot;
 
+import com.google.common.base.Throwables;
+import com.google.common.util.concurrent.AbstractExecutionThreadService;
+import com.google.common.util.concurrent.Service;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Talon;
 
-public class Robot extends IterativeRobot {
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+public class Robot extends IterativeRobot {
     private final Talon test;
+    private TeleOp teleOp;
 
     public Robot() {
+        super();
         test = new Talon(2);
+        teleOp = new TeleOp();
     }
 
     @Override
@@ -18,19 +29,45 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopInit() {
-        System.err.println("TeleOp Called");
-       test.set(1);
+        teleOp = new TeleOp();
+        teleOp.startAsync();
    }
 
     @Override
-    public  void disabledInit() {
-        test.set(0);
+    public void disabledInit() {
+        teleOp.stopAsync();
+        try {
+            teleOp.awaitTerminated(5, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            Throwables.propagate(e);
+        }
     }
 
     @Override
     public void teleopPeriodic() {
-        System.out.println("TeleOpP Called");
-        test.set(.9);
+        if (!teleOp.isRunning()) {
+            teleOp.startAsync();
+        }
+    }
+
+    private class TeleOp extends AbstractExecutionThreadService implements CoreRobotService {
+        @Override
+        public String getName() {
+            return "TeleOp";
+        }
+
+        @Override
+        public OperationalType getType() {
+            return OperationalType.TELEOP;
+        }
+
+
+        @Override
+        protected void run() throws Exception {
+            while (isRunning()) {
+                test.set(1);
+            }
+        }
     }
 }
 
